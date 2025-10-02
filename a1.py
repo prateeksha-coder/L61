dfeecc1b1049aff43343513eaab4b501ec9772f5
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-10-02 08:44:26 +0530

    commit

--- a1.py
+++ a1.py
@@ -3,7 +3,9 @@ num2 = 4
 print("num1 & num2 are:", num1, num2)
 print("num1 | num2 are:", num1 | num2)
 print("num1 ^ num2 are:", num1 ^ num2)
-print("num1 << num2 are:", num1 << num2)
-print("num1 >> num2 are:", num1 >> num2)
-print("~num1 are:", ~num1)
-print("~num2 are:", ~num2)
+#Using right shift operator
+print("num1 >> 1 are:", num1 >>1)
+print("num2 >> 1 are:", num2 >>1)
+#Using left shift operator
+print("num1 << 1 are:", num1 <<1)
+print("num2 << 1 are:", num2 <<1)
