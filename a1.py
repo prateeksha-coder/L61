c7ab4b3dbed4194e72e394f4dfee43f0edfcd746
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-11-09 14:01:20 +0530

    commit

--- a1.py
+++ a1.py
@@ -1,6 +1,6 @@
-num1 = 10 
-num2 = 4
-print("num1 & num2 are:", num1, num2)
+num1 = 10 #1010
+num2 = 4  #0100
+print("num1 & num2 are:", num1 & num2)
 print("num1 | num2 are:", num1 | num2)
 print("num1 ^ num2 are:", num1 ^ num2)
 #Using right shift operator
